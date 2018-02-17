da40d389ee91c0b79bd82b54ad858e9c5280fba8
Ravichandran Jv <ravjv2013@gmail.com>
2018-02-17 08:55:19 +0530

    Added message to src file

--- source/javascript/users_otp.js
+++ source/javascript/users_otp.js
@@ -5,6 +5,7 @@ module.exports={
 getUserWithOTP : function (otp) { 
 var url = 'mongodb://localhost:27017/mydb';
 return MongoClient.connect(url).then(function(db) {
+	console.log("Connected");
     var coll=db.collection('users');
 return coll.findOne({"otp":otp}).then(function(res){
 		return res.otp;
